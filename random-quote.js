92888213c2db1c73e7e1a866d5cdd2e7cfb3cc80
Jconfig <admin@joydeb.xyz>
2022-01-25 08:40:43 +0530

    Update random-quote.js
    Added more arrays of quotes

--- random-quote.js
+++ random-quote.js
@@ -2,8 +2,8 @@ console.log('Hello World!');
 
 function getQuote() {
   // Create the arrays
-  var quotes = new Array(8);
-  var sources = new Array(8);
+  var quotes = new Array(9);
+  var sources = new Array(9);
 
   // Initialize the arrays with quotes
   quotes[0] = "Optimism is the faith that leads to achievement.";
@@ -20,17 +20,20 @@ function getQuote() {
   quotes[3] = "What's another word for thesaurus?";
   sources[3] = "Steven Wright";
 
-  quotes[4] = "hsbs I am a Student of Hooghly *5555 HD TV and you ";
-  sources[4] = "HD TV ";
+  quotes[4] = "Attitudes are a secret power working 24 hours a day for good or bad.";
+  sources[4] = "Unknown";
 
-  quotes[5] = "KS for the least to come over and ";
-  sources[5] = "HS I am a ";
+  quotes[5] = "You can't always control circumstances. But you can control your own thoughts.";
+  sources[5] = "Charles Poppleston";
 
-  quotes[6] = "ls I am a Student of Hooghly ";
-  sources[6] = "JS framework which ";
+  quotes[6] = "There is nothing either good or bad thinking makes it so";
+  sources[6] = "William Shakespeare";
 
-  quotes[7] = "at the bottom of text add me on ";
-  sources[7] = "is the best award given ";
+  quotes[7] = "Better keep yourself clean and bright; you are the window through which you must see the world.";
+  sources[7] = "George Bernard Shaw";
+  
+  quotes[8] = "A happy person is not a person in a certain set of circumstances, but rather a person with a certain set of attitudes.";
+  sources[8] = "Hugh Downs";
 
   // Get a random index into the arrays
   i = Math.floor(Math.random() * quotes.length);
@@ -38,7 +41,7 @@ function getQuote() {
   // Write out the quote as HTML
   document.write("<p style='text-align: center;'>\"");
   document.write(quotes[i] + "\"");
-  document.write("<br /><em>- " + sources[i] + "</em>");
+  document.write("<br /><em>- " + sources[i] + "</em><span class="blink">...</span>");
   document.write("</p>");
 }
 // Stop hiding the script
